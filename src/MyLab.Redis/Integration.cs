3237914c0fc887444c1e9b090b9a7385866fbca5
Andrey O <ozzy@triasoft.com>
2020-10-22 02:08:13 +0300

    Add integration and configuration

---
+++ src/MyLab.Redis/Integration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyLab.Redis
+{
+    /// <summary>
+    /// Integration
+    /// </summary>
+    public static class RedisIntegration
+    {
+        public static IServiceCollection AddRedisUsage(this IServiceCollection services, IConfiguration configuration, string sectionName = "Redis")
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+            if (sectionName == null) throw new ArgumentNullException(nameof(sectionName));
+
+            return services.Configure<RedisOptions>(configuration.GetSection(sectionName));
+        }
+    }
+}
---
+++ src/MyLab.Redis/RedisOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace MyLab.Redis
+{
+    /// <summary>
+    /// Contains Redis configuration
+    /// </summary>
+    public class RedisOptions 
+    {
+        /// <summary>
+        /// Connection string
+        /// </summary>
+        /// <remarks>https://stackexchange.github.io/StackExchange.Redis/Configuration</remarks>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// Overrides password from <see cref="ConnectionString"/>
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
