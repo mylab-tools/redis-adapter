7e6451a66c43c1edb78f3adb1461ac604d50848e
Andrey O <ozzy@triasoft.com>
2020-10-22 21:15:00 +0300

    Add server level commands

--- src/MyLab.Redis/RedisServerToolsProvider.cs
+++ src/MyLab.Redis/RedisServerToolsProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using StackExchange.Redis;
 
@@ -20,9 +21,40 @@ namespace MyLab.Redis
         /// This command is often used to test if a connection is still alive, or to measure latency.
         /// </summary>
         /// <returns>The observed latency.</returns>
+        /// <remarks>https://redis.io/commands/ping</remarks>
         public async Task<TimeSpan> PingAsync()
         {
             return await _redisServer.PingAsync();
         }
+
+        /// <summary>
+        /// Return the same message passed in
+        /// </summary>
+        /// <param name="message">The message to echo.</param>
+        /// <remarks>https://redis.io/commands/echo</remarks>
+        public Task<RedisValue> EchoAsync(RedisValue message)
+        {
+            return _redisServer.EchoAsync(message);
+        }
+
+        /// <summary>
+        /// Returns all keys matching pattern; the KEYS or SCAN commands will be used based on the server capabilities; note: to resume an iteration via <i>cursor</i>, cast the original enumerable or enumerator to <i>IScanningCursor</i>.
+        /// </summary>
+        /// <param name="database">The database ID.</param>
+        /// <param name="pattern">The pattern to use.</param>
+        /// <param name="pageSize">The page size to iterate by.</param>
+        /// <param name="cursor">The cursor position to resume at.</param>
+        /// <param name="pageOffset">The page offset to start at.</param>
+        /// <param name="flags">The command flags to use.</param>
+        /// <remarks>Warning: consider KEYS as a command that should only be used in production environments with extreme care.</remarks>
+        /// <remarks>https://redis.io/commands/keys</remarks>
+        /// <remarks>https://redis.io/commands/scan</remarks>
+        public IAsyncEnumerable<RedisKey> KeysAsync(string pattern, int database = -1,
+            int pageSize = 250, long cursor = 0,
+            int pageOffset = 0)
+        {
+            return _redisServer.KeysAsync(database, pattern, pageSize, cursor, pageOffset);
+        }
+
     }
 }
