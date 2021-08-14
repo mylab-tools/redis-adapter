2ffd7041ba799caf9434c341d0b443bfc94c6147
Andrey O <ozzy.ext@gmail.com>
2021-08-14 23:14:49 +0300

    Remove transaction temurary

--- src/MyLab.Redis/RedisTransaction.cs
+++ src/MyLab.Redis/RedisTransaction.cs
@@ -1,28 +1,46 @@
-﻿using System;
-using System.Threading.Tasks;
-using StackExchange.Redis;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using StackExchange.Redis;
 
-namespace MyLab.Redis
-{
-    /// <summary>
-    /// Represent Redis transaction
-    /// </summary>
-    public class RedisTransaction : RedisDbKeysProvider, IAsyncDisposable
-    {
-        private readonly ITransaction _transaction;
+//namespace MyLab.Redis
+//{
+//    /// <summary>
+//    /// Represent Redis transaction
+//    /// </summary>
+//    public class RedisTransaction : IAsyncDisposable
+//    {
+//        private readonly ITransaction _transaction;
+//        private readonly RedisDbKeysProvider _db;
+//        private readonly List<Task> _tasks;
 
-        /// <summary>
-        /// Initializes a new instance of <see cref="RedisTransaction"/>
-        /// </summary>
-        public RedisTransaction(ITransaction transaction)
-            : base(transaction)
-        {
-            _transaction = transaction;
-        }
+//        /// <summary>
+//        /// Initializes a new instance of <see cref="RedisTransaction"/>
+//        /// </summary>
+//        public RedisTransaction(ITransaction transaction)
+//            :this(transaction, new RedisDbKeysProvider(transaction), new List<Task>())
+            
+//        {
+//        }
 
-        public ValueTask DisposeAsync()
-        {
-            return new ValueTask(_transaction. ExecuteAsync());
-        }
-    }
-}
+//        RedisTransaction(ITransaction transaction, RedisDbKeysProvider db, List<Task> initialTasks)
+
+//        {
+//            _transaction = transaction;
+//            _db = db;
+//            _tasks = initialTasks;
+//        }
+
+//        public RedisTransaction Enqueue(Func<RedisDbKeysProvider, Task> cmd)
+//        {
+//            return new RedisTransaction(_transaction, _db, new List<Task>(_tasks) { cmd(_db) });
+//        }
+
+//        public async ValueTask DisposeAsync()
+//        {
+
+//            if (await _transaction.ExecuteAsync())
+//                await Task.WhenAll(_tasks);
+//        }
+//    }
+//}
